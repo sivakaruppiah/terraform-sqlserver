bf8d8fb75ac1193e3342610603bff3f2815b3923
sivakaruppiah <44968911+sivakaruppiah@users.noreply.github.com>
2021-01-25 15:19:48 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@
 provider "azurerm" {
     # The "feature" block is required for AzureRM provider 2.x. 
     # If you're southing version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
     features {}
 
   # More information on the authentication methods supported by
